798b53ea85cf80ce4691a854d32d0f053a64a5a7
itzzzkay <kklegendofgaming@gmail.com>
2025-11-17 08:01:17 +0000

    fix vite

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/boldds.ke/" : "/",
+  base: "/boldds.ke/",
   plugins: [react()],
 });
